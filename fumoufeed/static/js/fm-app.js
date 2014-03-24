262a8f2824cc056571a4e915a26841f969de80cb
LittleQ <littleq0903@gmail.com>
2014-03-24 12:12:15 +0800

    added gitignore, and templates, and static files

---
+++ fumoufeed/static/js/fm-app.js
@@ -0,0 +1,15 @@
+FuMou = FuMou || {};
+
+
+FuMou.app = {
+    // Views
+    
+
+    
+    
+    init: function () {
+        // function to init all things
+        // TODO: init all views
+    
+    }
+}
