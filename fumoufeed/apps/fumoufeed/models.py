283bf832a80af815959e5f67803993e335e91940
george <goblin_george@gmail.com>
2014-03-24 14:53:43 +0800

    add model

--- fumoufeed/apps/fumoufeed/models.py
+++ fumoufeed/apps/fumoufeed/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class People(models.Model):
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=511)
+    fb_id = models.CharField(max_length=511)
 
+class Post(models.Model):
+    author = models.ForeignKey(People)
+    feed_id = models.CharField(max_length=511)
+    priority = models.IntegerField(default=10)
+    live = models.BooleanField(default=True)
+    create_time = models.DateTimeField(auto_now_add=True)
