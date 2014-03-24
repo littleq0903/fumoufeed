07999c9891587b43df929cf0470551727b65c0c8
george <goblin_george@gmail.com>
2014-03-24 15:36:00 +0800

    configure urls

--- fumoufeed/conf/urls.py
+++ fumoufeed/conf/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('apps.globals.urls', namespace="globals")),
 )
 
 if settings.DEBUG:
