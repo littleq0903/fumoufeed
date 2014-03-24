b6bfc54578509b772a68884e48cf996ed3de2fa1
george <goblin_george@gmail.com>
2014-03-24 19:26:35 +0800

    initial api

--- fumoufeed/apps/fumoufeed/models.py
+++ fumoufeed/apps/fumoufeed/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 
 class People(models.Model):
-    name = models.CharField(max_length=255)
+    fuid = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=511)
-    fb_id = models.CharField(max_length=511)
 
 class Post(models.Model):
     author = models.ForeignKey(People)
-    feed_id = models.CharField(max_length=511)
+    fpid = models.CharField(max_length=255, primary_key=True)
     priority = models.IntegerField(default=10)
     live = models.BooleanField(default=True)
     create_time = models.DateTimeField(auto_now_add=True)
