7bfc56e189ea1729a71796000f1d0edbb66a89ab
zorro0555 <zorro0555@gmail.com>
2014-03-25 19:13:08 +0800

    fix

---
+++ zorro.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+def main():
+    #if len(sys.argv) == 1:
+    #    return
+
+    #appId = sys.argv[1]
+    appId = "fumoufeed"
+    print "appId", appId
+
+    # Update Main System
+    # for macbook air environment
+    # cmd = r'python "C:\Program Files (x86)\Google\google_appengine\appcfg.py"'
+    # for normal environment
+    #cmd = r'"C:\Program Files (x86)\Google\google_appengine\appcfg.py"'
+    cmd = r'"appcfg.py"'
+    os.system(cmd + " update app.yaml " + " -A %s -e zorro0555@gmail.com --passin "%appId)
+    os.system("pause")
+
+
+if __name__ == "__main__":
+    main()
